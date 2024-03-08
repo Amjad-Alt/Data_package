965af945724b2308a7a92a8c99bb809f2ae1eedb
Amjad Altuwayjiri <amjadibrahim1994@gmail.com>
2024-03-08 14:21:53 -0500

    Create OCR.py

---
+++ code/OCR.py
@@ -0,0 +1,36 @@
+#%%
+import pytesseract
+from PIL import Image
+import pdf2image
+
+# Set the path for the Tesseract engine executable
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\amjad\\Downloads\\poppler-24.03.0.tar\\poppler-24.03.0'
+# Convert the first page of PDF to an image
+def convert_pdf_to_image(pdf_path):
+    # Here we convert only the first page
+    return pdf2image.convert_from_path(pdf_path, first_page=0, last_page=1)
+
+# Perform OCR on the images
+def ocr_images(images):
+    all_text = []
+    for img in images:
+        text = pytesseract.image_to_string(img, lang='eng')  # 'eng' for English, use different language codes as needed
+        all_text.append(text)
+    return all_text
+
+# Path to the PDF file
+pdf_path = '../data/2014.pdf'
+
+# Convert PDF to images
+images = convert_pdf_to_image(pdf_path)
+
+# Extract text from the images using OCR
+extracted_texts = ocr_images(images)
+
+# The OCR process is done, let's see the first 1000 characters of the extracted text
+print(extracted_texts[0][:1000])  # Print only the first 1000 characters to avoid too long output
+
+
+
+# %%
